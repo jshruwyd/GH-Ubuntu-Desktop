e11b4d1f9470c57cb45699ba6ed9983e28bddab4
Efebey2903 <swatibaljeet0101@gmail.com>
2024-06-06 12:57:17 +0300

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+echo Fixing and running vnc with web access
+
+for file in $(find /usr -type f -iname "*login1*"); do rm -rf $file 
+
+done
+
+websockify --web /usr/share/novnc/ --wrap-mode=ignore 6080 localhost:5900 
+echo "Starting VNC server..."
+vncserver :1 -geometry 2560x1440 -depth 24 -localhost -nolisten tcp -rfbport 5900
+echo "Done! Port 6080 and 5900 should now be forwarded. Simply open https://localhost:6080 to access the NoVNC client."
+             
 
 # Sonsuz döngü
 while true
