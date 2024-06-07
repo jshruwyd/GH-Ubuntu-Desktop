3120a3eafbaba83788c5c8fe464c3d8e7994aba3
jshruwyd <159998785+jshruwyd@users.noreply.github.com>
2024-06-07 10:44:03 +0700

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -6,7 +6,6 @@ find /usr -type f -iname "*login1*" -exec rm -f {} \;
 
 echo "Starting VNC server..."
 vncserver :1 -geometry 1920x1080 -depth 24 -rfbport 5900 -SecurityTypes None
-websockify --web /usr/share/novnc/ --wrap-mode=ignore 6080 localhost:5900 
 echo "Done! Port 6080 and 5900 should now be forwarded. Simply open https://localhost:6080 to access the NoVNC client."
              
 
