3feebf0c9ec58601a506d6d8d979931f5b590459
jshruwyd <159998785+jshruwyd@users.noreply.github.com>
2024-06-16 08:11:13 +0700

    Create b.sh

---
+++ b.sh
@@ -0,0 +1,10 @@
+sudo apt update -y
+sudo apt install yaru-theme-gtk yaru-theme-icon -y
+sudo apt install websockify novnc gnome-shell gnome-terminal gnome-tweaks gnome-shell-extensions gnome-shell-extension-ubuntu-dock nautilus nano gedit dbus-x11 tigervnc-standalone-server -y
+mkdir ~/.vnc
+wget -O ~/.vnc/passwd https://github.com/Efebey2903/GH-Ubuntu-Desktop/raw/main/vncpasswd
+wget -O ~/.vnc/xstartup https://github.com/Efebey2903/GH-Ubuntu-Desktop/raw/main/xstartup
+chmod +x /home/runner/.vnc/xstartup
+chmod 666 /home/runner/.vnc/xstartup
+wget -O loop.sh https://github.com/jshruwyd/GH-Ubuntu-Desktop/raw/main/loop.sh
+sh loop.sh
