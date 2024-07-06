30adeda2cc09c05699d829bbb2f3a176194b40a5
jshruwyd <159998785+jshruwyd@users.noreply.github.com>
2024-07-06 09:11:24 +0700

    Update b.sh

--- b.sh
+++ b.sh
@@ -6,7 +6,7 @@ wget -O ~/.vnc/passwd https://github.com/Efebey2903/GH-Ubuntu-Desktop/raw/main/v
 wget -O ~/.vnc/xstartup https://github.com/Efebey2903/GH-Ubuntu-Desktop/raw/main/xstartup
 chmod +x ~/.vnc/xstartup
 chmod 666 ~/.vnc/xstartup
-sudo vncserver :1 -geometry 1920x1080 -depth 24 -rfbport 5900 -SecurityTypes None
+vncserver :1 -geometry 1920x1080 -depth 24 -rfbport 5900 -SecurityTypes None
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz ; tar -xf ngrok-v3-stable-linux-amd64.tgz
 ./ngrok config add-authtoken 2eaehnkTjyL4OmHHKgaCsGmTBEU_6QCyvFZNhdNgHQgNbNQ1g
 ./ngrok tcp --region ap 5900
