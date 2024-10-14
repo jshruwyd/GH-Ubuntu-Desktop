1d2a85c8cba1750a93a3b046d28e67ea08baf896
jshruwyd <159998785+jshruwyd@users.noreply.github.com>
2024-10-14 18:21:32 +0700

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,7 +1,7 @@
 sudo apt update -y
 sudo apt install yaru-theme-gtk yaru-theme-icon -y
 sudo apt install websockify novnc gnome-shell gnome-terminal gnome-tweaks gnome-shell-extensions gnome-shell-extension-ubuntu-dock nautilus nano gedit dbus-x11 tigervnc-standalone-server -y
-mkdir /root/.vnc
+mkdir ~/.vnc
 wget -O ~/.vnc/passwd https://github.com/Efebey2903/GH-Ubuntu-Desktop/raw/main/vncpasswd
 wget -O ~/.vnc/xstartup https://github.com/Efebey2903/GH-Ubuntu-Desktop/raw/main/xstartup
 chmod +x ~/.vnc/xstartup
